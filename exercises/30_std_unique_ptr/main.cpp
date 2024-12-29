8fc7e10d6845a5233d39a560be089dddead69cf9
Guncuke <759398504@qq.com>
2024-12-29 13:02:00 +0800

    finish

--- exercises/30_std_unique_ptr/main.cpp
+++ exercises/30_std_unique_ptr/main.cpp
@@ -30,6 +30,7 @@ Unique drop(Unique ptr) {
     if (ptr) ptr->record('d');
     return nullptr;
 }
+
 Unique forward(Unique ptr) {
     if (ptr) ptr->record('f');
     return ptr;
@@ -38,23 +39,21 @@ Unique forward(Unique ptr) {
 int main(int argc, char **argv) {
     std::vector<std::string> problems[3];
 
-    drop(forward(reset(nullptr)));
+    drop(forward(reset(nullptr))); // new -> f -> d
     problems[0] = std::move(RECORDS);
-
+    // new -> f -> f -> r -> // -> d
     forward(drop(reset(forward(forward(reset(nullptr))))));
     problems[1] = std::move(RECORDS);
-
+    // new -> r // -> d -> r -> // -> d
     drop(drop(reset(drop(reset(reset(nullptr))))));
     problems[2] = std::move(RECORDS);
 
     // ---- 不要修改以上代码 ----
 
-    std::vector<const char *> answers[]{
+    std::vector<const char*> answers[] {
         {"fd"},
-        // TODO: 分析 problems[1] 中资源的生命周期，将记录填入 `std::vector`
-        // NOTICE: 此题结果依赖对象析构逻辑，平台相关，提交时以 CI 实际运行平台为准
-        {"", "", "", "", "", "", "", ""},
-        {"", "", "", "", "", "", "", ""},
+        {"d", "ffr"},
+        {"d", "d", "r"}
     };
 
     // ---- 不要修改以下代码 ----
